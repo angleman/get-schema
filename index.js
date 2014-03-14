e2fb172c9c93f5414ca7fc3929a1d4f7eecd4f03
angleman <angleman@cormea.com>
2014-03-13 18:02:49 -0600

    rework

--- index.js
+++ index.js
@@ -10,25 +10,30 @@ function GetSchema(client) {
 		client.query(qry, callback)
 	}
 	
-	self.get = function(tablename, callback) {
+	self.get = function(tablename, callback, addLength) {
 		function extractColumns(err, result) {
 			if (result && result.rows) {
 				rows = result.rows
-				result = []
+				result = {}
+				json   = []
+				slash  = ''
+				quote  = '"'
+				SQ     = slash + quote
 				for (i = 0; i < rows.length; i++) {
 					row = rows[i]
 					type = row['data_type']
+					typelen = (addLength) ? '(' + row['character_maximum_length'] + ')' : ''
 					switch (type) {
-					case 'character varying': type = 'varchar'; break
-					case 'character':         type = 'char';    break
+					case 'character varying': type = 'varchar' + typelen; break
+					case 'character':         type = 'char' + typelen;    break
 					case 'double precision':  type = 'float';   break
 					default: 
 						type = row['data_type'].split(' ')[0]
 					}
-					json = '{"'+row['column_name'] + '": "' + type + '"}'
-					row  = JSON.parse(json)
-					result.push(row)
+					json.push(SQ +row['column_name'] + SQ + ": " + SQ + type + SQ)
 				}
+				console.log(json)
+				result  = JSON.parse('{' + json.join(',') + '}')
 			}
 			callback(err, result)
 		}
@@ -36,28 +41,7 @@ function GetSchema(client) {
 	}
 	
 	self.getCreateTypes = function(tablename, callback) {
-		function extractColumns(err, result) {
-			if (result && result.rows) {
-				rows = result.rows
-				result = []
-				for (i = 0; i < rows.length; i++) {
-					row = rows[i]
-					type = row['data_type']
-					switch (type) {
-					case 'character varying': type = 'varchar(' + row['character_maximum_length'] + ')'; break
-					case 'character':         type = 'char(' + row['character_maximum_length'] + ')';    break
-					case 'double precision':  type = 'float';   break
-					default: 
-						type = row['data_type'].split(' ')[0]
-					}
-					json = '{"'+row['column_name'] + '": "' + type + '"}'
-					row  = JSON.parse(json)
-					result.push(row)
-				}
-			}
-			callback(err, result)
-		}
-		query(tablename, 'column_name, data_type, character_maximum_length', extractColumns)
+		return self.get(tablename, callback, true)
 	}
 	
 	self.getColumns = function(tablename, callback) {
